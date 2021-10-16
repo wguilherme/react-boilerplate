2ecc38490805c63c025ea515481d9c9ee97007a0
W. Guilherme <ravok123@gmail.com>
2021-10-16 12:47:00 -0300

    [update] add routes, form-hooks + register page

---
+++ src/pages/register/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+function Register() {
+
+
+
+  const { register, handleSubmit, watch, formState: { errors } } = useForm()
+  const onSubmit = data => console.log(data)
+
+  console.log(watch("example"))
+
+  return (
+    <div className="container">
+      <div className="form">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input type="text" {...register("example")} />
+          <input type="text" {...register("exampleRequired", { required: true })} />
+          {errors.exampleRequired && <span>This field is required</span>}
+          <input type="submit" />
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Register
+
