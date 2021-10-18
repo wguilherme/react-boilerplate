db29a08db5df9d83ebe56df982ac94576dfdbe90
W. Guilherme <ravok123@gmail.com>
2021-10-18 13:41:12 -0300

    [minor] button small changes

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,21 +4,22 @@
 interface ButtonProps {
   label?: string,
   size?: "small" | "medium" | "large"
-  submit: boolean
+  submit: any,
   onClick?: () => void;
 }
 
 const Button = ({
   size = "medium",
   label,
+  submit = "button",
   ...props
 }: ButtonProps) => {
 
   return (
     <div className="d-grid mx-auto ">
-      <button type="submit" className={`btn btn-primary btn-${size == "small" ? "sm" :
-        size == "medium" ? "md" : "lg"}`}>
-
+      <button type={submit}
+        className={`btn btn-primary btn-${size == "small" ? "sm" :
+          size == "medium" ? "md" : "lg"}`}>
         {label || "Confirmar"}
       </button>
     </div >
